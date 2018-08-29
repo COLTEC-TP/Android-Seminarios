c29a58b3e44f549bb870bb5ded91e62cdf6a6bbd
kingnowdo <36502982+kingnowdo@users.noreply.github.com>
2018-08-29 18:08:38 -0300

    Ação básica e setAutoCancel()

--- Appdenotificao/app/src/main/java/com/grupo/appnotificao/MainActivity.java
+++ Appdenotificao/app/src/main/java/com/grupo/appnotificao/MainActivity.java
@@ -3,7 +3,10 @@ package com.grupo.appnotificao;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +30,15 @@ public class MainActivity extends Activity {
                                 .setSmallIcon(R.drawable.ic_launcher_background)
                                 .setContentTitle("My notification")
                                 .setContentText("Hello World!");
+
+                Intent rIntent = new Intent(MainActivity.this, Main2Activity.class);
+                TaskStackBuilder sBuilder = TaskStackBuilder.create(MainActivity.this);
+                sBuilder.addParentStack(Main2Activity.class);
+                sBuilder.addNextIntent(rIntent);
+                PendingIntent rPendingIntent = sBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder.setContentIntent(rPendingIntent);
+
+                mBuilder.setAutoCancel(true); //Excluir automaticamente quando clicar
 
                 NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 mNotificationManager.notify(mId, mBuilder.build());
