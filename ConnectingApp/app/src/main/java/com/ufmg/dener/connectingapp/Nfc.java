e7acc38e36b8a7afdfd0ff6fe828802c3bbe3baa
Dener Vieira <deneribeiro10@hotmail.com>
2018-09-02 15:23:08 -0300

    bluetooth

---
+++ ConnectingApp/app/src/main/java/com/ufmg/dener/connectingapp/Nfc.java
@@ -0,0 +1,13 @@
+package com.ufmg.dener.connectingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nfc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+    }
+}
